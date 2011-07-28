39ee4324874172ea5bbf8ac2f5a78386a6575581
hmind <hmind2005@gmail.com>
2011-07-27 20:51:26 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myunischedule::Application.config.secret_token = 'f9fc24f44d58a3ebf52920b6560f113953a8da42a7d6e09ff38d02c40e3dda7e64a3328618035d678d6c59fe3c71130837a61f573ddb3a246328b39b63c1d685'
