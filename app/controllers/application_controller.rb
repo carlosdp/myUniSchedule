498191a8ee70abba2f8ddfc1893cf927f2fa29c8
hmind <hmind2005@gmail.com>
2011-07-29 13:01:08 -0400

    Added domain redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_filter :ensure_domain
+  
   protect_from_forgery
+  
+  APP_DOMAIN = ENV['APP_DOMAIN'] ? ENV['APP_DOMAIN'] : "localhost:3000"
+
+    def ensure_domain
+      if request.env['HTTP_HOST'] != APP_DOMAIN
+        # HTTP 301 is a "permanent" redirect
+        redirect_to "http://#{APP_DOMAIN}", :status => 301
+      end
+    end
 end
