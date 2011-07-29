306b11a3f98757b0aaffe1c40cea11865f9c528a
hmind <hmind2005@gmail.com>
2011-07-28 21:22:04 -0400

    Fixed redirect in OAuth

--- app/controllers/o_auth_controller.rb
+++ app/controllers/o_auth_controller.rb
@@ -2,7 +2,7 @@ class OAuthController < ApplicationController
   def redirect
     session[:access_token] = Koala::Facebook::OAuth.new("http://localhost:3000/o_auth/redirect").get_access_token(params[:code]) if params[:code]
     
-    redirect_to :controller => 'schedules', :action => 'index'
+    redirect_to root_path
   end
   
   def logout
